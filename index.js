0aec944caabebc6cedb9745f5054bb6e4a473a6d
Alina Kasianova <avkasyanova@gmail.com>
2021-08-13 17:36:42 +0300

    +

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const refs = {
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+    body: document.querySelector('body'),
+};
+
+let timerId = null;
+refs.startBtn.addEventListener("click", startColorChange);
+refs.stopBtn.addEventListener("click", stopColorChange);
+
+function startColorChange() {
+  timerId = setInterval(() => {
+      refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+  refs.startBtn.disabled = true;
+};
+
+function stopColorChange() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+  refs.body.style.backgroundColor = colors[0];
+};
